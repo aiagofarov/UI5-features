95c78845d9cf4d13fd3acf5055db0d5fd8828d75
aiagofarov <aleksandr.iagofarov@t-systems.com>
2020-02-11 13:42:33 +0300

    editable = "true" for every form

--- webapp/controller/Third.controller.js
+++ webapp/controller/Third.controller.js
@@ -3,20 +3,9 @@ sap.ui.define(["UI5-features/controller/BaseController"], function(BaseControlle
 
 	return BaseController.extend("UI5-features.controller.Third", {
 		onInit: function() {
-			//Attach MyJSONModel
-			//this.byId("tab2").setModel(this.getOwnerComponent().getModel('MyJSONModel'));
 		},
 
 		onClickOnItem: function(oEvent) {
-			//var oModel = this.getView().getModel("MyJSONModel");
-			//alert("Table item has been clicked!");
-
-			//Element binding
-			//var oItem = oEvent.getParameter('listItem');
-			//var oSelectedObject = oItem.getBindingContext().getObject();
-
-//			var oSelectedObject = oEvent.getSource().getBindingContext("MyJSONModel").getObject();
-			
 			var iId = oEvent.getSource().getBindingContext("MyJSONModel").getObject().id;
 			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
 			oRouter.navTo('fourthRoute', { entry_id: iId });
