78e11d6fa41076286d67772744daf34368f83127
aiagofarov <aleksandr.iagofarov@t-systems.com>
2020-02-11 19:16:16 +0300

    Mock data for ContactSet has been added but doesn't work :(

---
+++ webapp/localService/mock.js
@@ -0,0 +1,29 @@
+sap.ui.define(["sap/ui/core/util/mockserver"], function(MockServer) {
+	"use strict";
+
+	return {
+		mainServer: null,
+
+		init: function() {
+			var sPath = jQuery.sap.getModulePath("UI5-features.localService");
+			MockServer.config({
+				autoRespond: true,
+				autoRespondAfter: 1500
+			});
+
+			this.mainServer = new MockServer({
+				rootUrl: "/sap/opu/odata/iwbep/GWSAMPLE_BASIC/"
+			});
+
+			this.mainServer.simulate(sPath + "/metadata.xml", {
+				sMockdataBaseUrl: sPath + "/Data",
+				bGenerateMissingMockData: true
+			});
+			this.mainServer.start();
+		},
+
+		stop: function() {
+			this.mainServer.stop();
+		}
+	};
+});
